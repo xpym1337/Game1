d79dc860d807577eb2ae52cafc659945aa3dcb68
xpym1337 <xpym.binkler@gmail.com>
2025-08-17 12:29:14 +1000

    Add GAS modules to build dependencies
    - Added GameplayAbilities, GameplayTags, GameplayTasks to PublicDependencyModuleNames
    - Enables project to use Gameplay Ability System classes
    - Ready for implementing AbilitySystemComponent and abilities"

--- Source/EROEOREOREOR/EROEOREOREOR.Build.cs
+++ Source/EROEOREOREOR/EROEOREOREOR.Build.cs
@@ -8,7 +8,7 @@ public class EROEOREOREOR : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "GameplayAbilities", "GameplayTags", "GameplayTasks" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
